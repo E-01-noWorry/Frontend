23b7d3653c0a7d53cb344bbc5ecc4b8edf46c101
devyouth94 <devyouth94@gmail.com>
2022-09-03 16:26:43 +0900

    [style] CSS 기초 작업

---
+++ src/shared/themes/iconStyle.js
@@ -0,0 +1,38 @@
+import { css } from 'styled-components';
+
+//아이콘 사이즈
+export const IconExtra = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 4rem;
+  height: 4rem;
+`;
+
+export const IconLarge = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 2.8rem;
+  height: 2.8rem;
+`;
+
+export const IconMedium = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+export const IconSmall = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 2rem;
+  height: 2rem;
+`;
---
+++ src/shared/themes/textStyle.js
@@ -0,0 +1,27 @@
+import { css } from 'styled-components';
+
+//폰트 사이즈
+export const fontExtra = css`
+  font-size: 3.2rem;
+`;
+
+export const fontLarge = css`
+  font-size: 2rem;
+`;
+
+export const fontMedium = css`
+  font-size: 1.4rem;
+`;
+
+export const fontSmall = css`
+  font-size: 1.3rem;
+`;
+
+//폰트 두께
+export const fontExtraBold = css`
+  font-weight: 700;
+`;
+
+export const fontBold = css`
+  font-weight: 500;
+`;
