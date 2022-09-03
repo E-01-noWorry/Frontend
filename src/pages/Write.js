6bf1b3f7fc6c255896f0321508e720a53c9dd6a4
devyouth94 <devyouth94@gmail.com>
2022-09-03 11:57:56 +0900

    [update] 작성 페이지 수정 1. 채팅방 생성 페이지 추가(skeleton) 2. 그 외 주석 추가 및 자잘한 수정

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import WriteRoom from '../components/features/write/WriteRoom';
 import WriteSelect from '../components/features/write/WriteSelect';
 
 const Write = () => {
   const { state } = useLocation();
 
-  return <div>{state === 'select' ? <WriteSelect /> : null}</div>;
+  return <div>{state === 'select' ? <WriteSelect /> : <WriteRoom />}</div>;
 };
 
 export default Write;
