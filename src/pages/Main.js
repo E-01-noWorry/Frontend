97f6bfcd89eb217c99460311dcb6ded70dd8632d
devyouth94 <devyouth94@gmail.com>
2022-09-07 11:10:00 +0900

    [fix] 코드 오타 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import MainSelect from '../components/features/main/MainSelect';
 import MainRoom from '../components/features/main/MainRoom';
