bb974016238e3249e33e0686e8837da598d867a5
devyouth94 <devyouth94@gmail.com>
2022-09-07 22:40:35 +0900

    [update] 작성페이지 관련 수정 1. 시간설정 파일 분리 2. 코드 정리 3. 해시태그 기능 추가

--- src/components/features/write/WriteRoom.js
+++ src/components/features/write/WriteRoom.js
@@ -1,14 +1,21 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+
 import instance from '../../../app/module/instance';
+
+import BodyPadding from '../../common/BodyPadding';
+import Header from '../../common/Header';
+import GlobalButton from '../../elements/GlobalButton';
+
 import { borderBoxDefault } from '../../../shared/themes/boxStyle';
-import { IconLarge } from '../../../shared/themes/iconStyle';
+import { IconLarge, IconSmall } from '../../../shared/themes/iconStyle';
 import {
   fontBold,
   fontMedium,
   fontSmall,
 } from '../../../shared/themes/textStyle';
+
+import styled from 'styled-components';
 
 const WriteRoom = () => {
   const navigate = useNavigate();
@@ -21,14 +28,20 @@ const WriteRoom = () => {
 
   const keywordHandler = (event) => {
     event.preventDefault();
-    if (keywordArr.includes(keyword)) {
-      alert('중복된 해시태그가 있네요');
-    } else if (keywordArr.length >= 3) {
+
+    if (keywordArr.length >= 3) {
       alert('해시태그는 3개까지만이에요');
+    } else if (keywordArr.includes(keyword)) {
+      alert('중복된 해시태그가 있네요');
     } else {
-      setKeywordArr((prev) => [...prev, keyword]);
+      setKeywordArr((prev) => [...prev, keyword.replace(' ', '')]);
     }
+
     setKeyword('');
+  };
+
+  const keywordDeleteHandler = (value) => {
+    setKeywordArr((prev) => prev.filter((i) => i !== value));
   };
 
   const countPlusHandler = () => {
@@ -49,6 +62,7 @@ const WriteRoom = () => {
       hashTag: keywordArr,
       max: countPeople + 1,
     };
+
     try {
       const { data } = await instance.post('/room', payload);
       navigate(`/chatroom/${data.result.roomKey}`);
@@ -59,64 +73,76 @@ const WriteRoom = () => {
 
   return (
     <>
-      <StHeader>
+      <Header>
         <StHeaderIcon
           onClick={() => navigate('/', { state: 'room' })}
         ></StHeaderIcon>
         <h1>채팅방 만들기</h1>
         <StHeaderIcon />
-      </StHeader>
+      </Header>
 
-      <StContainer>
-        <StContentBox>
-          <StInnerTitle>고민 상담방 이름 작성</StInnerTitle>
-          <StInnerText>
-            <textarea
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-              maxLength="40"
-              placeholder="고민 상담방 이름을 작성해주세요"
-              style={{ height: '8.9rem' }}
-            />
-            <span>{title.length}/40자</span>
-          </StInnerText>
-        </StContentBox>
-
-        <StContentBox>
-          <StInnerTitle>해시태그 작성</StInnerTitle>
-          <StInnerText>
-            <form onSubmit={keywordHandler}>
-              <input
-                value={keyword}
-                onChange={(event) => setKeyword(event.target.value)}
-                maxLength={10}
-                placeholder="해시태그를 작성하여 고민을 소개해보세요(10자 이내)"
+      <BodyPadding>
+        <StContainer>
+          <StContentBox>
+            <StInnerTitle>고민 상담방 이름 작성</StInnerTitle>
+            <StInnerText>
+              <textarea
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                maxLength={20}
+                placeholder="고민 상담방 이름을 작성해주세요"
+                style={{ height: '8.9rem' }}
               />
-            </form>
-            <div>
-              {keywordArr?.map((item) => (
-                <div key={item}>
-                  <span>{item}</span>
-                </div>
-              ))}
-            </div>
-            <span>{keywordArr.length}/3개</span>
-          </StInnerText>
-        </StContentBox>
+              <span>{title.length}/20자</span>
+            </StInnerText>
+          </StContentBox>
 
-        <StContentBox>
-          <StInnerTitle>최대 참여 인원수</StInnerTitle>
-          <StInnerSubtitle>
-            본인을 제외한 참여자 인원을 정해주세요
-          </StInnerSubtitle>
-          <StCountBox>
-            <StCountButton onClick={countMinusHandler}>-</StCountButton>
-            <div>{countPeople}</div>
-            <StCountButton onClick={countPlusHandler}>+</StCountButton>
-          </StCountBox>
-        </StContentBox>
-        <StButton onClick={submitHandler}>만들기</StButton>
-      </StContainer>
+          <StContentBox>
+            <StInnerTitle>해시태그 작성</StInnerTitle>
+            <StInnerText>
+              <StKeywordWrap length={keywordArr.length}>
+                {keywordArr?.map((item) => (
+                  <StKeyword
+                    key={item}
+                    onClick={() => keywordDeleteHandler(item)}
+                  >
+                    <div>#{item}</div>
+                    <StDeleteIcon></StDeleteIcon>
+                  </StKeyword>
+                ))}
+                <form onSubmit={keywordHandler}>
+                  <input
+                    value={keyword}
+                    onChange={(event) => setKeyword(event.target.value)}
+                    maxLength={7}
+                    placeholder={
+                      keywordArr.length === 0
+                        ? '해시태그를 작성하여 고민을 소개해보세요(7자 이내)'
+                        : '해시태그 추가'
+                    }
+                  />
+                </form>
+              </StKeywordWrap>
+
+              <span>{keywordArr.length}/3개</span>
+            </StInnerText>
+          </StContentBox>
+
+          <StContentBox>
+            <StInnerTitle>최대 참여 인원수</StInnerTitle>
+            <StInnerSubtitle>
+              본인을 제외한 참여자 인원을 정해주세요
+            </StInnerSubtitle>
+            <StCountBox>
+              <StCountButton onClick={countMinusHandler}>-</StCountButton>
+              <div>{countPeople}</div>
+              <StCountButton onClick={countPlusHandler}>+</StCountButton>
+            </StCountBox>
+          </StContentBox>
+
+          <GlobalButton onClick={submitHandler}>만들기</GlobalButton>
+        </StContainer>
+      </BodyPadding>
     </>
   );
 };
@@ -126,19 +152,11 @@ export default WriteRoom;
 const StContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  margin-top: 1.7rem;
-
   gap: 3.2rem;
-`;
-
-const StHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 
   width: 100%;
-  height: 6.4rem;
+  margin-top: 8.1rem;
+  margin-bottom: 1.6rem;
 `;
 
 const StHeaderIcon = styled.div`
@@ -152,30 +170,107 @@ const StContentBox = styled.div`
 `;
 
 const StInnerTitle = styled.div`
+  margin-bottom: 1.6rem;
+
   ${fontBold};
   line-height: 2.4rem;
+`;
 
-  height: 2.4rem;
-  margin-bottom: 1.6rem;
+const StInnerText = styled.div`
+  position: relative;
+
+  ${borderBoxDefault};
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  height: 100%;
+  min-height: 12.1rem;
+  padding: 1.6rem;
+  background-color: #ededed;
+
+  textarea {
+    width: 100%;
+    height: 6.9rem;
+    background-color: transparent;
+
+    resize: none;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  //글자수 표시
+  & > span {
+    position: absolute;
+    top: 8.6rem;
+    right: 1.6rem;
+
+    ${fontSmall};
+    line-height: 2rem;
+  }
+`;
+
+const StKeywordWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 0.8rem;
+  row-gap: 1rem;
+
+  input {
+    display: ${(props) => (props.length >= 3 ? 'none' : 'block')};
+
+    width: ${(props) => (props.length === 0 ? '210%' : null)};
+    height: 3.2rem;
+    background-color: transparent;
+
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+const StKeyword = styled.div`
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  height: 3.2rem;
+  padding: 1.1rem;
+  background-color: #fff;
+
+  border-radius: 1.6rem;
+
+  div {
+    ${fontMedium};
+  }
+`;
+
+const StDeleteIcon = styled.div`
+  ${IconSmall}
+  background-color: green;
 `;
 
 const StInnerSubtitle = styled.div`
+  margin-top: -1.2rem;
+
   ${fontMedium};
   line-height: 2.1rem;
-
-  margin-top: -1.2rem;
 `;
 
 const StCountBox = styled.div`
   ${borderBoxDefault};
-  height: 7.6rem;
-  padding: 1.4rem;
-  margin-top: 2rem;
-
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
 
+  height: 7.6rem;
+  padding: 1.4rem;
+  margin-top: 2rem;
   background-color: #ededed;
 `;
 
@@ -186,68 +281,7 @@ const StCountButton = styled.div`
 
   width: 4.8rem;
   height: 4.8rem;
-  border-radius: 1.4rem;
-
   background-color: #fff;
-`;
 
-const StInnerText = styled.div`
-  position: relative;
-  ${borderBoxDefault};
-  height: 100%;
-  min-height: 12.1rem;
-  align-items: flex-start;
-  padding: 1.6rem;
-
-  background-color: #ededed;
-
-  textarea {
-    width: 100%;
-    height: 6.9rem;
-
-    background-color: transparent;
-    resize: none;
-    border: none;
-
-    &:focus {
-      outline: none;
-    }
-  }
-
-  input {
-    position: absolute;
-    top: 1.8rem;
-
-    width: 90%;
-    background-color: transparent;
-    border: none;
-
-    &:focus {
-      outline: none;
-    }
-  }
-
-  & > span {
-    ${fontSmall};
-    line-height: 2rem;
-
-    position: absolute;
-    top: 8.6rem;
-    right: 1.6rem;
-  }
-`;
-
-const StButton = styled.div`
-  background-color: #000;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.9rem;
-
-  width: 100%;
-  height: 5.6rem;
-  border-radius: 2rem;
-
-  ${fontBold};
-  color: #fff;
+  border-radius: 1.4rem;
 `;
--- src/components/features/write/WriteSelect.js
+++ src/components/features/write/WriteSelect.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import instance from '../../../app/module/instance';
+
+import GlobalButton from '../../elements/GlobalButton';
+import BodyPadding from '../../common/BodyPadding';
+import Header from '../../common/Header';
 import WriteImageUpload from './WriteImageUpload';
-import { TIME_ARR, CATEGORY_ARR } from '../../../shared/Array';
-import styled from 'styled-components';
+import TimeSlide from './TimeSlide';
+
+import { CATEGORY_ARR } from '../../../shared/Array';
+
 import {
   fontBold,
   fontMedium,
@@ -12,11 +19,15 @@ import {
 import { borderBoxDefault } from '../../../shared/themes/boxStyle';
 import { IconLarge, IconMedium } from '../../../shared/themes/iconStyle';
 
+import styled from 'styled-components';
+
 const WriteSelect = () => {
   const navigate = useNavigate();
 
-  //서버에 전송할 payload를 관리하는 State
   const [numArr, setNumArr] = useState([1, 2]);
+  const [modal, setModal] = useState('');
+
+  //서버에 전송할 payload
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
   const [options, setOptions] = useState({ 1: '', 2: '' });
@@ -39,12 +50,6 @@ const WriteSelect = () => {
     setNumArr(numArr.filter((num) => num !== payload));
     setOptions({ ...options, [payload]: '' });
     setImages((prev) => ({ ...prev, [payload]: '' }));
-  };
-
-  //마감시간 설정 핸들러
-  const timeHandler = (event) => {
-    const time = event.target.getAttribute('time');
-    setTime(time);
   };
 
   //고민 게시글 작성 POST API
@@ -90,136 +95,104 @@ const WriteSelect = () => {
 
   return (
     <>
-      <StHeader>
+      <Header>
         <StHeaderIcon
           onClick={() => navigate('/', { state: 'select' })}
         ></StHeaderIcon>
         <h1>투표 만들기</h1>
         <StHeaderIcon />
-      </StHeader>
+      </Header>
 
-      <StContainer>
-        <StContentBox>
-          <StInnerTitle>고민 작성</StInnerTitle>
-          <StInnerText>
-            <textarea
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-              maxLength="80"
-              placeholder="고민을 작성해주세요"
-              style={{ height: '8.9rem' }}
-            />
-            <span>{title.length}/80자</span>
-          </StInnerText>
-        </StContentBox>
+      <BodyPadding>
+        <StContainer>
+          <StContentBox>
+            <StInnerTitle>고민 작성</StInnerTitle>
+            <StInnerText>
+              <textarea
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                maxLength={40}
+                placeholder="고민을 작성해주세요"
+                style={{ height: '8.9rem' }}
+              />
+              <span>{title.length}/40자</span>
+            </StInnerText>
+          </StContentBox>
 
-        <StContentBox>
-          <StInnerTitle>카테고리 선택</StInnerTitle>
-          <StInnerCategory>
-            {CATEGORY_ARR.map((item) => (
-              <div key={item} onClick={() => setCategory(item)}>
-                <input
-                  type="radio"
-                  id={item}
-                  checked={category === item}
-                  onChange={() => setCategory(item)}
-                />
-                <label htmlFor={item}>{item}</label>
-              </div>
-            ))}
-          </StInnerCategory>
-        </StContentBox>
-
-        <StContentBox>
-          <StInnerTitle>선택지 작성</StInnerTitle>
-          <StInnerOptions>
-            {numArr.map((num, idx) => (
-              <div key={num}>
-                <StInnerSubtitle>
-                  <div>선택지 {idx + 1}</div>
-
-                  {numArr.length > 2 && (
-                    <div onClick={() => optionDeleteHandler(num)}>삭제</div>
-                  )}
-                </StInnerSubtitle>
-                <StInnerText>
-                  <textarea
-                    name={num}
-                    value={options[num]}
-                    onChange={optionChangeHandler}
-                    maxLength="40"
-                    placeholder="선택지를 작성해주세요"
+          <StContentBox>
+            <StInnerTitle>카테고리 선택</StInnerTitle>
+            <StInnerCategory>
+              {CATEGORY_ARR.map((item) => (
+                <div key={item} onClick={() => setCategory(item)}>
+                  <input
+                    type="radio"
+                    id={item}
+                    checked={category === item}
+                    onChange={() => setCategory(item)}
                   />
-                  <span>{options[num]?.length || '0'}/40자</span>
-                  <WriteImageUpload setImages={setImages} num={num} />
-                </StInnerText>
-              </div>
-            ))}
-          </StInnerOptions>
-        </StContentBox>
+                  <label htmlFor={item}>{item}</label>
+                </div>
+              ))}
+            </StInnerCategory>
+          </StContentBox>
 
-        <StContentBox>
-          {numArr.length < 4 && (
-            <StButton onClick={optionAddHandler} bgColor={'#fff'}>
-              <StPlusIcon></StPlusIcon>
-              <span>선택지 추가하기</span>
-            </StButton>
-          )}
-        </StContentBox>
+          <StContentBox>
+            <StInnerTitle>선택지 작성</StInnerTitle>
+            <StInnerOptions>
+              {numArr.map((num, idx) => (
+                <div key={num}>
+                  <StInnerSubtitle>
+                    <div>선택지 {idx + 1}</div>
 
-        <StContentBox>
-          <StInnerTitle>투표 종료시간 선택</StInnerTitle>
-          <StInnerButton selectTime={time}>
-            {TIME_ARR.map((time) => (
-              <div key={time} time={time} onClick={timeHandler}>
-                {time}시간
-              </div>
-            ))}
-            <StInnerTime time={time}></StInnerTime>
-          </StInnerButton>
-        </StContentBox>
+                    {numArr.length > 2 && (
+                      <div onClick={() => optionDeleteHandler(num)}>삭제</div>
+                    )}
+                  </StInnerSubtitle>
+                  <StInnerText>
+                    <textarea
+                      name={num}
+                      value={options[num]}
+                      onChange={optionChangeHandler}
+                      maxLength={20}
+                      placeholder="선택지를 작성해주세요"
+                    />
+                    <span>{options[num]?.length || '0'}/20자</span>
+                    <WriteImageUpload setImages={setImages} num={num} />
+                  </StInnerText>
+                </div>
+              ))}
+            </StInnerOptions>
+          </StContentBox>
 
-        <StInnerSubmit>
-          <span>투표는 등록 후 수정이 불가능하니 유의해 주세요</span>
-          <StButton onClick={submitHandler} bgColor={'#000'} fontColor={'#fff'}>
-            등록하기
-          </StButton>
-        </StInnerSubmit>
-      </StContainer>
+          <StContentBox>
+            {numArr.length < 4 && (
+              <GlobalButton
+                onClick={optionAddHandler}
+                bgc={'#fff'}
+                color={'#000'}
+              >
+                <StPlusIcon></StPlusIcon>
+                <span>선택지 추가하기</span>
+              </GlobalButton>
+            )}
+          </StContentBox>
+
+          <StContentBox>
+            <StInnerTitle>투표 종료시간 선택</StInnerTitle>
+            <TimeSlide time={time} setTime={setTime} />
+          </StContentBox>
+
+          <StInnerSubmit>
+            <span>투표는 등록 후 수정이 불가능하니 유의해 주세요</span>
+            <GlobalButton onClick={submitHandler}>등록하기</GlobalButton>
+          </StInnerSubmit>
+        </StContainer>
+      </BodyPadding>
     </>
   );
 };
 
 export default WriteSelect;
-
-const StButton = styled.div`
-  background-color: ${(props) => props.bgColor};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.9rem;
-
-  width: 100%;
-  height: 5.6rem;
-  border-radius: 2rem;
-
-  ${fontBold};
-  color: ${(props) => props.fontColor || '#000'};
-`;
-
-const StPlusIcon = styled.div`
-  ${IconMedium};
-  background-color: green;
-`;
-
-const StHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  width: 100%;
-  height: 6.4rem;
-`;
 
 const StHeaderIcon = styled.div`
   ${IconLarge};
@@ -229,10 +202,11 @@ const StHeaderIcon = styled.div`
 const StContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  margin-top: 1.7rem;
-
   gap: 3.2rem;
+
+  width: 100%;
+  margin-top: 8.1rem;
+  margin-bottom: 1.6rem;
 `;
 
 const StContentBox = styled.div`
@@ -241,28 +215,28 @@ const StContentBox = styled.div`
 `;
 
 const StInnerTitle = styled.div`
+  margin-bottom: 1.6rem;
+
   ${fontBold};
   line-height: 2.4rem;
-
-  height: 2.4rem;
-  margin-bottom: 1.6rem;
 `;
 
 const StInnerText = styled.div`
   position: relative;
+
   ${borderBoxDefault};
+  align-items: flex-start;
+
   height: 100%;
   min-height: 12.1rem;
-  align-items: flex-start;
   padding: 1.6rem;
-
   background-color: #ededed;
 
   textarea {
     width: 100%;
     height: 6.9rem;
-
     background-color: transparent;
+
     resize: none;
     border: none;
 
@@ -272,27 +246,28 @@ const StInnerText = styled.div`
   }
 
   & > span {
-    ${fontSmall};
-    line-height: 2rem;
-
     position: absolute;
     top: 8.6rem;
     right: 1.6rem;
+
+    ${fontSmall};
+    line-height: 2rem;
   }
 `;
 
 const StInnerCategory = styled.div`
   ${borderBoxDefault};
   align-items: flex-start;
-  height: 100%;
 
+  height: 100%;
   background-color: #fff;
 
   div {
     display: flex;
+    align-items: center;
+
     width: 100%;
     height: 5.6rem;
-    align-items: center;
     padding: 0 1.1rem;
 
     input {
@@ -318,117 +293,34 @@ const StInnerSubtitle = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   width: 100%;
 
   div {
+    margin-bottom: 1.6rem;
+
     ${fontSmall}
     ${fontBold}
     line-height: 2.1rem;
-
-    height: 2.1rem;
-    margin-bottom: 1.6rem;
   }
 `;
 
-const StInnerButton = styled.div`
-  position: relative;
-  display: flex;
-
-  div {
-    ${fontMedium}
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition-delay: 0.1s;
-    width: 20%;
-    height: 4rem;
-    background-color: transparent;
-    border: none;
-  }
-
-  div:nth-child(1) {
-    color: #fff;
-    ${fontBold}
-  }
-
-  div:nth-child(2) {
-    color: ${(props) => props.selectTime != 1 && '#fff'};
-    ${(props) => props.selectTime != 1 && fontBold};
-  }
-
-  div:nth-child(3) {
-    color: ${(props) =>
-      props.selectTime != 1 && props.selectTime != 4 && '#fff'};
-    ${(props) => props.selectTime != 1 && props.selectTime != 4 && fontBold};
-  }
-
-  div:nth-child(4) {
-    color: ${(props) =>
-      props.selectTime != 1 &&
-      props.selectTime != 4 &&
-      props.selectTime != 8 &&
-      '#fff'};
-    ${(props) =>
-      props.selectTime != 1 &&
-      props.selectTime != 4 &&
-      props.selectTime != 8 &&
-      fontBold};
-  }
-
-  div:nth-child(5) {
-    color: ${(props) =>
-      props.selectTime != 1 &&
-      props.selectTime != 4 &&
-      props.selectTime != 8 &&
-      props.selectTime != 12 &&
-      '#fff'};
-    ${(props) =>
-      props.selectTime != 1 &&
-      props.selectTime != 4 &&
-      props.selectTime != 8 &&
-      props.selectTime != 12 &&
-      fontBold};
-  }
-`;
-
-const StInnerTime = styled.span`
-  background-color: #454545;
-  position: absolute;
-  display: inline-block;
-  top: 0;
-  left: 0;
-  transition-duration: 0.3s;
-  width: ${(props) => {
-    if (props.time == 1) {
-      return '20%';
-    } else if (props.time == 4) {
-      return '40%';
-    } else if (props.time == 8) {
-      return '60%';
-    } else if (props.time == 12) {
-      return '80%';
-    } else if (props.time == 24) {
-      return '100%';
-    }
-  }};
-  height: 4rem;
-  border-radius: 2rem;
-  z-index: -1;
+const StPlusIcon = styled.div`
+  ${IconMedium};
+  background-color: green;
 `;
 
 const StInnerSubmit = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-
   align-items: center;
 
-  margin-bottom: 1.6rem;
+  width: 100%;
 
   span {
-    ${fontSmall}
-    height: 2rem;
-    line-height: 1.95rem;
     margin-bottom: 1.6rem;
+
+    ${fontSmall}
+    line-height: 2rem;
   }
 `;
