42b235a7d26e1d03bcd026d78c8ce7efa0c40931
devyouth94 <devyouth94@gmail.com>
2022-09-07 22:37:58 +0900

    [update] 공통 컴포넌트 수정 및 코드 정리

--- src/components/elements/WriteButton.js
+++ src/components/elements/WriteButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styled from 'styled-components';
 
 const WriteButton = ({ onClick }) => {
@@ -11,9 +12,11 @@ const StWriteButton = styled.button`
   position: fixed;
   bottom: 8rem;
   right: 2.4rem;
-  background-color: #9b9b9b;
+
   width: 6.4rem;
   height: 6.4rem;
+  background-color: #9b9b9b;
+
   border: none;
   border-radius: 50%;
 `;
