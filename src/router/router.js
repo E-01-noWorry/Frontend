2a610dc53bdf29612686deed4a2d6db39911aca0
devyouth94 <devyouth94@gmail.com>
2022-09-09 02:36:10 +0900

    [add] 시작 화면 페이지 추가

--- src/router/router.js
+++ src/router/router.js
@@ -1,30 +1,34 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+const Welcome = lazy(() => import('../pages/Welcome'));
 const Main = lazy(() => import('../pages/Main'));
-const Login = lazy(() => import('../pages/Login'));
 const SignUp = lazy(() => import('../pages/SignUp'));
+const Login = lazy(() => import('../pages/Login'));
+const KakaoCode = lazy(() => import('../components/features/KakaoCode'));
+const MyPage = lazy(() => import('../pages/MyPage'));
+const PostVoted = lazy(() => import('../components/features/mypage/postVoted'));
 const Write = lazy(() => import('../pages/Write'));
 const Detail = lazy(() => import('../pages/Detail'));
 const ChatRoom = lazy(() => import('../pages/ChatRoom'));
-const MyPage = lazy(() => import('../pages/MyPage'));
-const KakaoCode = lazy(() => import('../components/features/KakaoCode'));
-const PostVoted = lazy(() => import('../components/features/mypage/postVoted'));
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/main" element={<Main />} />
+
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/api/auth/kakao/callback" element={<KakaoCode />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/postvoted" element={<PostVoted />} />
+
           <Route path="/write" element={<Write />} />
           <Route path="/detail/:selectKey" element={<Detail />} />
           <Route path="/chatroom/:roomKey" element={<ChatRoom />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/postvoted" element={<PostVoted />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
