973a80edf1880d05ae4cdfcdf3b4873c6a233455
devyouth94 <devyouth94@gmail.com>
2022-09-12 10:01:57 +0900

    [update] 미들웨어 수정

--- src/app/config/store.js
+++ src/app/config/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import signUpSlice from '../module/signUpSlice';
 import loginSlice from '../module/loginSlice';
 import select from '../module/selectSlice';
