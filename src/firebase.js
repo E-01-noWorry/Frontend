3bb22c1bc2f7998a20770e1075413834cf80f686
devyouth94 <devyouth94@gmail.com>
2022-09-20 17:47:11 +0900

    [fix] pwa 오류 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_FCM_API_KEY}`,
+  apiKey: 'AIzaSyDt_B4xdUHmXE3c1KzUIaGXnu6DF1DPv6o',
   authDomain: 'gomgom-5f801.firebaseapp.com',
   projectId: 'gomgom-5f801',
   storageBucket: 'gomgom-5f801.appspot.com',
-  messagingSenderId: `${process.env.REACT_APP_SENDERID}`,
-  appId: `${process.env.REACT_APP_FCM_APP_ID}`,
-  measurementId: `${process.env.REACT_APP_FCM_MEASUREMENT_ID}`,
+  messagingSenderId: '648358836971',
+  appId: '1:648358836971:web:8324e3bcd408694e605cae',
+  measurementId: 'G-L26D3ED5XE',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
