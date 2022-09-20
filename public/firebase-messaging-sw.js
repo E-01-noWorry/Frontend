a88a24fbfd02d250faaea8d7cc377b489cb8c625
devyouth94 <devyouth94@gmail.com>
2022-09-20 16:17:45 +0900

    [update] 알람 코드 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,34 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts(
+  'https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js',
+);
 
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: 'AIzaSyDt_B4xdUHmXE3c1KzUIaGXnu6DF1DPv6o',
+  authDomain: 'gomgom-5f801.firebaseapp.com',
+  projectId: 'gomgom-5f801',
+  storageBucket: 'gomgom-5f801.appspot.com',
+  messagingSenderId: '648358836971',
+  appId: '1:648358836971:web:8324e3bcd408694e605cae',
+  measurementId: 'G-L26D3ED5XE',
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const firebaseMessaging = firebase.messaging();
+
+firebaseMessaging.onBackgroundMessage((payload) => {
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
