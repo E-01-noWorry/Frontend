7abcc4f28ca377d425dc9d688ec4bebddf6b2203
devyouth94 <devyouth94@gmail.com>
2022-10-08 23:26:52 +0900

    [update] 코드 스플리팅 적용

--- src/router/router.js
+++ src/router/router.js
@@ -1,49 +1,27 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Loading from '../pages/Loading';
-import Start from '../pages/Start';
-import OnBoarding from '../pages/OnBoarding';
+const Loading = lazy(() => import('../pages/Loading'));
+const Start = lazy(() => import('../pages/Start'));
+const OnBoarding = lazy(() => import('../pages/OnBoarding'));
 
-import SignUp from '../pages/SignUp';
-import Login from '../pages/Login';
-import KakaoRedirect from '../pages/KakaoRedirect';
-import GoogleRedirect from '../pages/GoogleRedirect';
+const SignUp = lazy(() => import('../pages/SignUp'));
+const Login = lazy(() => import('../pages/Login'));
+const KakaoRedirect = lazy(() => import('../pages/KakaoRedirect'));
+const GoogleRedirect = lazy(() => import('../pages/GoogleRedirect'));
 
-import Main from '../pages/Main';
-import Answer from '../pages/Answer';
+const Main = lazy(() => import('../pages/Main'));
+const Answer = lazy(() => import('../pages/Answer'));
 
-import MyPage from '../pages/mypage/MyPage';
-import PostVoted from '../pages/mypage/PostVoted';
-import Voted from '../pages/mypage/Voted';
-import MadeRoom from '../pages/mypage/MadeRoom';
-import OperatingRoom from '../pages/mypage/OperatingRoom';
+const MyPage = lazy(() => import('../pages/mypage/MyPage'));
+const PostVoted = lazy(() => import('../pages/mypage/PostVoted'));
+const Voted = lazy(() => import('../pages/mypage/Voted'));
+const MadeRoom = lazy(() => import('../pages/mypage/MadeRoom'));
+const OperatingRoom = lazy(() => import('../pages/mypage/OperatingRoom'));
 
-import Write from '../pages/Write';
-import Detail from '../pages/Detail';
-import ChatRoom from '../pages/ChatRoom';
-
-// const Loading = lazy(() => import('../pages/Loading'));
-// const Start = lazy(() => import('../pages/Start'));
-// const OnBoarding = lazy(() => import('../pages/OnBoarding'));
-
-// const SignUp = lazy(() => import('../pages/SignUp'));
-// const Login = lazy(() => import('../pages/Login'));
-// const KakaoRedirect = lazy(() => import('../pages/KakaoRedirect'));
-// const GoogleRedirect = lazy(() => import('../pages/GoogleRedirect'));
-
-// const Main = lazy(() => import('../pages/Main'));
-// const Answer = lazy(() => import('../pages/Answer'));
-
-// const MyPage = lazy(() => import('../pages/mypage/MyPage'));
-// const PostVoted = lazy(() => import('../pages/mypage/PostVoted'));
-// const Voted = lazy(() => import('../pages/mypage/Voted'));
-// const MadeRoom = lazy(() => import('../pages/mypage/MadeRoom'));
-// const OperatingRoom = lazy(() => import('../pages/mypage/OperatingRoom'));
-
-// const Write = lazy(() => import('../pages/Write'));
-// const Detail = lazy(() => import('../pages/Detail'));
-// const ChatRoom = lazy(() => import('../pages/ChatRoom'));
+const Write = lazy(() => import('../pages/Write'));
+const Detail = lazy(() => import('../pages/Detail'));
+const ChatRoom = lazy(() => import('../pages/ChatRoom'));
 
 const Router = () => {
   return (
