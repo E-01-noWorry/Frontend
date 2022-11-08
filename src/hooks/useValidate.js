4de1b8cc54487b6e628f30368e49a38a9119b91a
devyouth94 <devyouth94@gmail.com>
2022-11-08 20:36:01 +0900

    Refactor: 유효성 검사 훅스 및 상수 파일로 분리

---
+++ src/hooks/useValidate.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import {
+  isValidateConfirm,
+  isValidateNickname,
+  isValidatePassword,
+  isValidateUserId,
+} from "shared/utils/validate";
+
+const initialValue = {
+  userId: "",
+  password: "",
+  confirm: "",
+  nickname: "",
+};
+
+const useSignUpInput = () => {
+  const [signUpInfo, setSignUpInfo] = useState(initialValue);
+  const [validateInfo, setValidateInfo] = useState({
+    ...initialValue,
+    nickname: "*익명으로 안심하고 고민을 이야기할 수 있어요",
+  });
+
+  const handleVlidate = (name, value) => {
+    switch (name) {
+      case "userId":
+        setValidateInfo((prev) => ({ ...prev, userId: isValidateUserId(value) }));
+        break;
+      case "password":
+        setValidateInfo((prev) => ({ ...prev, password: isValidatePassword(value) }));
+        break;
+      case "confirm":
+        setValidateInfo((prev) => ({
+          ...prev,
+          confirm: isValidateConfirm(signUpInfo.password, value),
+        }));
+        break;
+      case "nickname":
+        setValidateInfo((prev) => ({ ...prev, nickname: isValidateNickname(value) }));
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleOnChange = (event) => {
+    const { value, name } = event.target;
+    setSignUpInfo({ ...signUpInfo, [name]: value });
+    handleVlidate(name, value);
+  };
+
+  return [signUpInfo, validateInfo, handleOnChange];
+};
+
+export default useSignUpInput;
