8bf0c67aa51dadfcbcfab4164760cc52207c4bc7
devyouth94 <devyouth94@gmail.com>
2022-11-08 17:09:37 +0900

    Refactor: 헤더 컴포넌트 코드 정리

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,43 @@
+import { fontLarge } from "shared/themes/textStyle";
+import styled from "styled-components";
+
+const Header = ({ children, ...rest }) => {
+  return <S.Header {...rest}>{children}</S.Header>;
+};
+
+export default Header;
+
+const S = {
+  Header: styled.header`
+    @media ${({ theme }) => theme.device.PC} {
+      left: ${({ theme }) => theme.style.left};
+      transform: ${({ theme }) => theme.style.transform};
+
+      width: ${({ theme }) => theme.style.width};
+    }
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    display: grid;
+    grid-template-columns: 3.2rem auto 3.2rem;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    height: 6.4rem;
+    padding: 0 2rem;
+    background-color: ${({ theme }) => theme.bg};
+
+    z-index: 9;
+
+    > img {
+      cursor: pointer;
+    }
+
+    > h1 {
+      ${fontLarge}
+    }
+  `,
+};
