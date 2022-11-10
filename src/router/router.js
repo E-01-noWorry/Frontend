99e5fe84b0a82e739a7092c2583d8ababb19dd07
devyouth94 <devyouth94@gmail.com>
2022-11-11 02:50:06 +0900

    Refactor: 새로 작업중인 페이지 라우터에 추가

--- src/router/router.js
+++ src/router/router.js
@@ -1,27 +1,28 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const Loading = lazy(() => import('../pages/Loading'));
-const Start = lazy(() => import('../pages/Start'));
-const OnBoarding = lazy(() => import('../pages/OnBoarding'));
+const Loading = lazy(() => import("pages/Loading"));
+const Start = lazy(() => import("pages/Start"));
+const OnBoarding = lazy(() => import("../pages/OnBoarding"));
 
-const SignUp = lazy(() => import('../pages/SignUp'));
-const Login = lazy(() => import('../pages/Login'));
-const KakaoRedirect = lazy(() => import('../pages/KakaoRedirect'));
-const GoogleRedirect = lazy(() => import('../pages/GoogleRedirect'));
+const SignUp = lazy(() => import("pages/SignUp"));
+const Login = lazy(() => import("pages/Login"));
+const KakaoRedirect = lazy(() => import("pages/KakaoRedirect"));
+const GoogleRedirect = lazy(() => import("pages/GoogleRedirect"));
 
-const Main = lazy(() => import('../pages/Main'));
-const Answer = lazy(() => import('../pages/Answer'));
+const Main = lazy(() => import("pages/Main"));
+const Select = lazy(() => import("pages/Select"));
+const Answer = lazy(() => import("pages/Answer"));
 
-const MyPage = lazy(() => import('../pages/mypage/MyPage'));
-const PostVoted = lazy(() => import('../pages/mypage/PostVoted'));
-const Voted = lazy(() => import('../pages/mypage/Voted'));
-const MadeRoom = lazy(() => import('../pages/mypage/MadeRoom'));
-const OperatingRoom = lazy(() => import('../pages/mypage/OperatingRoom'));
+const MyPage = lazy(() => import("pages/mypage/MyPage"));
+const PostVoted = lazy(() => import("pages/mypage/PostVoted"));
+const Voted = lazy(() => import("pages/mypage/Voted"));
+const MadeRoom = lazy(() => import("pages/mypage/MadeRoom"));
+const OperatingRoom = lazy(() => import("pages/mypage/OperatingRoom"));
 
-const Write = lazy(() => import('../pages/Write'));
-const Detail = lazy(() => import('../pages/Detail'));
-const ChatRoom = lazy(() => import('../pages/ChatRoom'));
+const Write = lazy(() => import("pages/Write"));
+const Detail = lazy(() => import("pages/Detail"));
+const ChatRoom = lazy(() => import("pages/ChatRoom"));
 
 const Router = () => {
   return (
@@ -34,12 +35,10 @@ const Router = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/api/auth/kakao/callback" element={<KakaoRedirect />} />
-          <Route
-            path="/api/auth/google/callback"
-            element={<GoogleRedirect />}
-          />
+          <Route path="/api/auth/google/callback" element={<GoogleRedirect />} />
 
           <Route path="/main" element={<Main />} />
+          <Route path="/select" element={<Select />} />
           <Route path="/answer" element={<Answer />} />
 
           <Route path="/mypage" element={<MyPage />} />
