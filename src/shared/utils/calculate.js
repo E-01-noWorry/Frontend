496c7ee7a35030b9480d0c8b72f0f7bb7bf54b86
devyouth94 <devyouth94@gmail.com>
2022-11-12 02:26:28 +0900

    Refactor: 상수값 추가 및 수정

---
+++ src/shared/utils/calculate.js
@@ -0,0 +1,29 @@
+const colorFromPoint = (point) => {
+  if (0 <= point && point <= 10) {
+    return "White";
+  } else if (11 <= point && point <= 25) {
+    return "Yellow";
+  } else if (26 <= point && point <= 50) {
+    return "Blue";
+  } else if (51 <= point && point <= 100) {
+    return "Purple";
+  } else {
+    return null;
+  }
+};
+
+const remainedPoint = (point) => {
+  if (0 <= point && point <= 10) {
+    return 11 - point;
+  } else if (11 <= point && point <= 25) {
+    return 26 - point;
+  } else if (26 <= point && point <= 50) {
+    return 51 - point;
+  } else if (51 <= point && point <= 100) {
+    return 101 - point;
+  } else {
+    return "-";
+  }
+};
+
+export { colorFromPoint, remainedPoint };
--- src/shared/utils/localStorage.js
+++ src/shared/utils/localStorage.js
@@ -5,6 +5,22 @@ const userStorage = {
     localStorage.setItem("nickname", userData.nickname);
     localStorage.setItem("userKey", userData.userKey);
   },
+
+  clearStorage: () => {
+    localStorage.clear();
+  },
+
+  getToken: () => {
+    return localStorage.getItem("accessToken");
+  },
+
+  getNickname: () => {
+    return localStorage.getItem("nickname");
+  },
+
+  setNickname: (nickname) => {
+    localStorage.setItem("nickname", nickname);
+  },
 };
 
 Object.freeze(userStorage);
